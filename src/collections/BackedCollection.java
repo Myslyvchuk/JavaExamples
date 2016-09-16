4af0a332e5c8856ae326c07e065803aef56fb441
Bogdan Myslyvchuk <bmys@softserveinc.com>
2016-09-16 16:13:05 +0300

    ibtresting cases, concurrency

--- src/collections/BackedCollection.java
+++ src/collections/BackedCollection.java
@@ -1,47 +1,54 @@
 package collections;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Hashtable;
 import java.util.LinkedHashMap;
 import java.util.LinkedHashSet;
+import java.util.List;
 import java.util.Set;
 import java.util.SortedMap;
 import java.util.TreeMap;
 
 public class BackedCollection {
-    public static void main(String[] args) {
-        TreeMap<String, String> map = new TreeMap<String, String>();
-        HashMap<String, Integer> hash = new LinkedHashMap<String, Integer>();
-        Hashtable<String, Integer> hashTable = new Hashtable<String, Integer>();
-        hash.put("FB", 1);
-        hash.put(null, 1);
-        map.put("a", "ant");
-        map.put("d", "dog");
-        map.put("h", "horse");
-        hashTable.put("Ea", 1);
-        System.out.println(map +"0--------" );
-        SortedMap<String, String> submap;
-        submap = map.subMap("a", "z"); // #1 create a backed collection RANGE ON KEY FROM B-M
-        System.out.println(map + " " + submap +"1--------"); // #2 show contents
-        map.put("b", "bat"); // #3 add to original
-        submap.put("f", "fish"); // #4 add to copy
-        map.put("r", "raccoon"); // #5 add to original - out of range
-        // submap.put("p", "pig"); // #6 add to copy - out of range
-        System.out.println(map + " " + submap+"2--------"); // #7 show final contents
-        System.out.println(map + " map--------"); // #7 show final contents
-        System.out.println(submap+"submap--------"); // #7 show final contents
-        map.put("g", "gireafe");
-        submap.put("ha", "gireafe");
-        System.out.println(map + " map--------"); // #7 show final contents
-        System.out.println(submap+"submap--------"); // #7 show final contents
-        
-        BigDecimal a = new BigDecimal(1);
-        BigDecimal b = new BigDecimal(0.2f);
-        System.out.println(a.subtract(b));
-        
-        Set<String> set = new LinkedHashSet<String>();
-        set.add("FB");
-        set.add("Ea");
-    }
+	public static void main(String[] args) {
+		TreeMap<String, String> map = new TreeMap<String, String>();
+		HashMap<String, Integer> hash = new LinkedHashMap<String, Integer>();
+		Hashtable<String, Integer> hashTable = new Hashtable<String, Integer>();
+		hash.put("FB", 1);
+		hash.put(null, 1);
+		map.put("a", "ant");
+		map.put("d", "dog");
+		map.put("h", "horse");
+		hashTable.put("Ea", 1);
+		System.out.println(map + "0--------");
+		SortedMap<String, String> submap;
+		submap = map.subMap("a", "z"); // #1 create a backed collection RANGE ON
+										// KEY FROM B-M
+		System.out.println(map + " " + submap + "1--------"); // #2 show
+																// contents
+		map.put("b", "bat"); // #3 add to original
+		submap.put("f", "fish"); // #4 add to copy
+		map.put("r", "raccoon"); // #5 add to original - out of range
+		// submap.put("p", "pig"); // #6 add to copy - out of range
+		System.out.println(map + " " + submap + "2--------"); // #7 show final
+																// contents
+		System.out.println(map + " map--------"); // #7 show final contents
+		System.out.println(submap + "submap--------"); // #7 show final contents
+		map.put("g", "gireafe");
+		submap.put("ha", "gireafe");
+		System.out.println(map + " map--------"); // #7 show final contents
+		System.out.println(submap + "submap--------"); // #7 show final contents
+
+		BigDecimal a = new BigDecimal(1);
+		BigDecimal b = new BigDecimal(0.2f);
+		System.out.println(a.subtract(b));
+
+		Set<String> set = new LinkedHashSet<String>();
+		set.add("FB");
+		set.add("Ea");
+		
+	}
+
 }
---
+++ src/intresting/IntrestingCase.java
@@ -0,0 +1,68 @@
+package intresting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class IntrestingCase {
+	/**
+	 * 1.How to write method that receive list of child objects
+	 */
+	class A {
+	}
+
+	class B extends A {
+	}
+
+	static List<A> aList = new ArrayList<>();
+	static List<B> bList = new ArrayList<>();
+
+	public static void listInsertion(List<? extends A> inheritanceList) {
+
+	}
+
+	public static void main(String[] args) {
+		/**
+		 * 2. How to insert in collection type other than generic
+		 */
+		List<Integer> integer = new ArrayList<Integer>();
+		integer.add(1);
+		List objects = integer;
+		objects.add("String");
+		System.out.println(integer);
+		/**
+		 * 1.How to write method that receive list of child objects
+		 */
+		listInsertion(aList);
+		listInsertion(bList);
+		/**
+		 * MultiDemensional Array initialization
+		 */
+		int[][] arrayDimensional = new int[2][];
+		arrayDimensional[0] = new int[5];
+		arrayDimensional[0][0] = 1;
+		arrayDimensional[0][1] = 2;
+		arrayDimensional[0][2] = 3;
+		arrayDimensional[0][3] = 4;
+		arrayDimensional[0][4] = 5;
+		arrayDimensional[1] = new int[5];
+		arrayDimensional[1][0] = 1;
+		arrayDimensional[1][1] = 2;
+		arrayDimensional[1][2] = 3;
+		arrayDimensional[1][3] = 4;
+		arrayDimensional[1][4] = 5;
+		// ArrayIndexOutOfBoundsException
+		// arrayDimensional[2] = new int[5];
+		// arrayDimensional[2][0] = 1;
+		// arrayDimensional[2][1] = 2;
+		// arrayDimensional[2][2] = 3;
+		// arrayDimensional[2][3] = 4;
+		// arrayDimensional[2][4] = 5;
+
+		for (int[] inner : arrayDimensional) {
+			for (int num : inner)
+				System.out.print(num + " ");
+			System.out.println();
+		}
+	}
+
+}
